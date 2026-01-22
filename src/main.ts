5ad54cc8f3ee5d68abf34fd2d39ad84ec2f867f0
nicoperez22 <nicoperez2295@gmail.com>
2026-01-21 21:26:53 -0300

    Cambios 2

--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,19 @@ async function bootstrap() {
     origin: [
       'http://localhost:4200',
       'http://localhost:5173',
-      'https://TU-FRONTEND.com',
+      'https://blueviolet-dunlin-800160.hostingersite.com/',
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-  const options = new DocumentBuilder()
-    .setTitle('API - Sean Eternos')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  // const options = new DocumentBuilder()
+  //   .setTitle('API - Sean Eternos')
+  //   .setVersion('1.0')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('api', app, document);
 
   app.setGlobalPrefix('api/v1');
   const port = Number(process.env.PORT) || 3000;
